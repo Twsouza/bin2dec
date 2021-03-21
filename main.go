b6d1395d126d3c58f13bf7bde1c722e43b26cf4b
Taynan Souza <tsouza@87labs.com>
2021-03-21 18:22:23 -0300

    (feature) User must be notified if anything other than a 0 or 1 was entered
    If the string is not a valid binary, it logs to user.

--- main.go
+++ main.go
@@ -3,6 +3,11 @@ package main
 import (
 	"fmt"
 	"log"
+	"regexp"
+)
+
+var (
+	isBinary = regexp.MustCompile("^[0-1]+$").MatchString
 )
 
 func main() {
@@ -10,6 +15,10 @@ func main() {
 	log.Println("Enter the binary:")
 	if _, err := fmt.Scanf("%s", &binarys); err != nil {
 		log.Fatalf("Could not read from input, error %v", err)
+	}
+
+	if !isBinary(binarys) {
+		log.Fatalf("%s is not a valid binary!", binarys)
 	}
 
 	log.Println("You entered ", binarys)
