0f3b2f3318f8944778085087646de1af93c7acd3
Taynan Souza <tsouza@87labs.com>
2021-03-21 18:15:22 -0300

    (feature) User can enter the binary in input
    Once the program is started, it will ask the binary.

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+)
 
+func main() {
+	var binarys string
+	log.Println("Enter the binary:")
+	if _, err := fmt.Scanf("%s", &binarys); err != nil {
+		log.Fatalf("Could not read from input, error %v", err)
+	}
+
+	log.Println("You entered ", binarys)
 }
